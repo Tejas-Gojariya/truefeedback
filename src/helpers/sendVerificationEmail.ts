1a7d0fea5ebd1b1c27178406a95f3511cd771c5d
Tejas Gojariya <tejasgojariya036@gmail.com>
2025-01-22 16:55:17 +0530

    add domin name

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -9,7 +9,7 @@ export async function sendVerificationEmail(
 ) : Promise<ApiResponse>{
     try {
         await resend.emails.send({
-            from : 'onboarding@resend.dev',
+            from : 'no-reply@truefeedback.site',
             to : email,
             subject : 'TrueFeedBack | Verification code',
             react : VerificationEmail({username, otp: verifyCode})
