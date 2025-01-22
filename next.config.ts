82d4b6df2f5110164a9a67cd8be8b5c14b3c846e
Tejas Gojariya <tejasgojariya036@gmail.com>
2025-01-22 17:46:58 +0530

    add ignoreBuildErrors in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    typescript: {
+        ignoreBuildErrors: true,
+    },
 
-module.exports = nextConfig
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+};
+
+module.exports = nextConfig;
